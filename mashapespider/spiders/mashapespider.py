fdda7e734a2429c0d0ac00e3bcc3b628c18cddc9
Giorgos Margaritis <gmargari@cs.uoi.gr>
2016-03-24 13:37:46 +0200

    First working version of Mashapi spider

--- mashapespider/spiders/mashapespider.py
+++ mashapespider/spiders/mashapespider.py
@@ -1,6 +1,9 @@
 import scrapy
 import json
 import sys
+from selenium import webdriver
+from scrapy import signals
+from scrapy.xlib.pydispatch import dispatcher
 
 #===============================================================================
 # MashapeWebSpider
@@ -15,6 +18,46 @@ class MashapeWebSpider(scrapy.Spider):
     download_delay = 0.25
 
     #===========================================================================
+    # __init__ ()
+    #===========================================================================
+    def __init__(self):
+        self.api_varnames_xpaths = {
+            'name': "//h1[contains(@class,'name')]",
+            'owner': "//div[contains(@class,'owner')]",
+            'website': "//div[contains(@class,'website')]",
+            'tags': "//div[contains(@class,'tags')]",
+            'description': "//span[contains(@class,'more-contaner')]",
+        }
+
+        self.endpoints_xpath = "//section[@class='endpoint']"
+        self.endpoint_varnames_xpaths = {
+            'name': ".//div[@class='request']/div[@class='endpoint-name']/span",
+            'description': ".//div[@class='request']/div[@class='description']",
+            'method': ".//div[@class='response']/pre/div/span[@class='verb']",
+            'host': ".//div[@class='response']/pre/div/span/span[@class='host']",
+            'route': ".//div[@class='response']/pre/div/span/span[@class='route']",
+        }
+
+        self.params_xpath = ".//div[@class='request']/div[contains(@class, 'parameter') and contains(@class, 'typed')]"
+        self.param_varnames_xpaths = {
+            'name': ".//span[contains(@class, 'name')]",
+            'type': ".//span[contains(@class, 'type')]",
+            'description': ".//p",
+        }
+
+        # Initialized our headless browser
+        self.browser = webdriver.PhantomJS()
+
+        # Function to be called when spider terminates
+        dispatcher.connect(self.quit, signals.spider_closed)
+
+    #===========================================================================
+    # quit ()
+    #===========================================================================
+    def quit(self):
+        self.browser.quit()
+
+    #===========================================================================
     # parse ()
     #===========================================================================
     def parse(self, response):
@@ -24,7 +67,7 @@ class MashapeWebSpider(scrapy.Spider):
     # parse_api_directory_page ()
     #===========================================================================
     def parse_api_directory_page(self, response):
-        # Parse current directory page
+        # For each API link found in current directory page call parse_api_page
         for tr in response.xpath("//tr[@class='list-api-row']"):
             url = tr.xpath("td[2]/a/@href").extract()[0]
             fullurl = response.urljoin(url)
@@ -34,14 +77,38 @@ class MashapeWebSpider(scrapy.Spider):
     # parse_api_page ()
     #===========================================================================
     def parse_api_page(self, response):
-        print response.url
-        return
-        d = dict()
-        for div in response.xpath("//div[@id='tabs-content']/div[2]/div[@class='field']"):
-            key = str(div.xpath("label/text()").extract()[0])
+        self.browser.get(response.url)
+
+        # Get API description
+        api = dict()
+        api['mashape_url'] = response.url
+        self.add_elements_to_dict_if_existing(api, self.browser, self.api_varnames_xpaths)
+
+        # Get API endpoints
+        api['endpoints'] = list()
+        for elem_endpoint in self.browser.find_elements_by_xpath(self.endpoints_xpath):
+
+            # Get endpoint description
+            endpoint = dict()
+            self.add_elements_to_dict_if_existing(endpoint, elem_endpoint, self.endpoint_varnames_xpaths)
+
+            # Get endpoint params
+            endpoint['params'] = list()
+            for elem_param in elem_endpoint.find_elements_by_xpath(self.params_xpath):
+                param = dict()
+                self.add_elements_to_dict_if_existing(param, elem_param, self.param_varnames_xpaths)
+                param['required'] = "true" if "required" in elem_param.get_attribute("class") else "false"
+                endpoint['params'].append(param)
+            api['endpoints'].append(endpoint)
+
+        print json.dumps(api, sort_keys=True)
+
+    #===========================================================================
+    # add_elements_to_dict_if_existing ()
+    #===========================================================================
+    def add_elements_to_dict_if_existing(self, dictionary, dom_element, varnames_xpaths):
+        for varname, xpath in varnames_xpaths.items():
             try:
-                value = str(div.xpath("span/a/text()").extract()[0])
+                dictionary[varname] = dom_element.find_elements_by_xpath(xpath)[0].text
             except:
-                value = str(div.xpath("span/text()").extract()[0])
-            d[key] = value
-        print json.dumps(d, sort_keys=True)
+                pass
