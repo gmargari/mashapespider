8713434f01e93cb1b4a4dcc43280689093423e95
Giorgos Margaritis <gmargari@cs.uoi.gr>
2016-03-30 15:40:49 +0300

    Parse request headers

--- mashapespider/spiders/mashapespider.py
+++ mashapespider/spiders/mashapespider.py
@@ -83,6 +83,14 @@ class MashapeWebSpider(scrapy.Spider):
             'response_example': ".//pre[contains(@class, 'model-preview')]/div[@class='perfectscroll-container']",
         }
 
+        # All parameter divs after <h4>Request Headers</h4> with class "parameter typed authorization"
+        self.request_headers_xpath = ".//div[@class='request']/h4[contains(., 'Request Headers')][1]/following-sibling::div[contains(@class, 'parameter') and contains(@class, 'typed') and contains(@class, 'authorization')]"
+        self.request_headers_varnames_xpaths = {
+            'name': ".//span[contains(@class, 'name')]",
+            'type': ".//span[contains(@class, 'type')]",
+            'description': ".//p",
+        }
+
         # All parameter divs after <h4>URL Parameters</h4> and before the 2nd <h4> (Url params is always first <h4> within request div)
         self.url_params_xpath = ".//div[@class='request']/h4[contains(., 'URL Parameters')][1]/following-sibling::div[contains(@class, 'parameter') and contains(@class, 'typed')][count(preceding-sibling::h4)=1]"
         self.url_param_varnames_xpaths = {
@@ -189,6 +197,13 @@ class MashapeWebSpider(scrapy.Spider):
                 param = dict()
                 self.add_elements_to_dict_if_existing(param, elem_param, self.request_payload_varnames_xpaths)
                 endpoint['payload'].append(param)
+
+            # Get endpoint request headers
+            endpoint['request_headers'] = list()
+            for elem_param in elem_endpoint.find_elements_by_xpath(self.request_headers_xpath):
+                param = dict()
+                self.add_elements_to_dict_if_existing(param, elem_param, self.request_headers_varnames_xpaths)
+                endpoint['request_headers'].append(param)
 
             # Get endpoint URL params
             endpoint['url_params'] = list()
